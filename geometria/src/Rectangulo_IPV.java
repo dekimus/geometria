01d68b50ad19ccea280705f3e160664cd79b8e91
Israel Pérez <deki.wii@gmail.com>
2022-02-24 16:40:56 +0100

    versión comentada

--- geometria/src/Rectangulo_IPV.java
+++ geometria/src/Rectangulo_IPV.java
@@ -1,19 +1,40 @@
  
+/**
+ * 
+ * @author Israel Pérez Valero
+ * @version 1.2
+ * 
+ */
 public class Rectangulo_IPV extends FiguraGeometrica_IPV {
 	private double l1;
 	private double l2;
 	
+	/**
+	 * Constructor de la clase
+	 * @param recibe dos dobles
+	 * 
+	 */
 	public Rectangulo_IPV(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
-
+	
+	/**
+	 * Calcula el área del rectángulo
+	 * @return devuelve un doble
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	
+	/**
+	 * Calcula el perímetro del retángulo
+	 * @return devuelve un doble
+	 * 
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
